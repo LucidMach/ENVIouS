27bf84cf15def1a549e6f00a6c0f3cb78d19503d
lucidmach <lucidmach@gmail.com>
2023-01-18 17:56:46 +0900

    slider for motor control

---
+++ src/components/slider.tsx
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction } from "react";
+
+interface props {
+  value: number;
+  setValue: Dispatch<SetStateAction<number>>;
+}
+const Slider: React.FC<props> = ({ value, setValue }) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <input
+        type="range"
+        min="-100"
+        max="100"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+        step="5"
+        className="absolute w-[500px] h-2 -rotate-90 bg-yellow-500 rounded-lg appearance-none cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,37 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { useEffect, useState } from "react";
+import Slider from "@/components/slider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [imgB64, setImgB64] = useState<string>("");
+
   const [prevTime, setPrev] = useState(Date.now());
   const [count, setCount] = useState(0);
   const [fps, setFPS] = useState(0);
 
+  const [RMotor, setRMotor] = useState<number>(0);
+  const [LMotor, setLMotor] = useState<number>(0);
+
   const ws_endpoint = "ws://192.168.128.90:5000/";
 
+  // get cam feeds
   useEffect(() => {
-    const ws = new WebSocket(ws_endpoint);
+    const ws_cam = new WebSocket(ws_endpoint);
 
-    ws.onopen = () => {
+    ws_cam.onopen = () => {
       console.log("handshake successful [listener]");
-      ws.send("READY");
+      ws_cam.send("READY");
 
-      ws.onmessage = (img) => {
+      ws_cam.onmessage = (img) => {
         setImgB64(img.data);
-        ws.send("READY");
+        ws_cam.send("READY");
       };
     };
 
-    ws.onclose = () => {
+    ws_cam.onclose = () => {
       console.log("disconnected [listener]");
     };
   }, []);
@@ -40,6 +46,11 @@ export default function Home() {
     }
   }, [imgB64]);
 
+  // send motor values
+  useEffect(() => {
+    console.log({ LMotor, RMotor });
+  }, [RMotor, LMotor]);
+
   return (
     <>
       <Head>
@@ -48,13 +59,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-slate-900 w-full h-full flex justify-center items-center">
-        <h1 className="absolute top-0 right-0 bg-black">FPS: {fps}</h1>
+      <main className="bg-slate-900 w-full h-full flex justify-evenly items-center">
+        <Slider value={LMotor} setValue={setLMotor} />
+        <h1 className="absolute bottom-0 bg-slate-900 px-4 m-4 rounded-full">
+          FPS: {fps}
+        </h1>
         <img
           src={imgB64}
-          className="w-full h-full"
+          className="p-3 rounded-3xl h-full"
           alt="live image from rasPi"
         />
+        <Slider value={RMotor} setValue={setRMotor} />
       </main>
     </>
   );
