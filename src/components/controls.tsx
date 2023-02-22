e3f99968223de2b4773298ac352d87d5174303d2
lucidmach <lucidmach@gmail.com>
2023-02-22 15:20:37 +0900

    controls trigger keydown

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -22,46 +22,47 @@ const Controls: React.FC<props> = ({
   const [bg, __] = useAtom(bgAtom);
   return (
     <div
-      className={`absolute flex flex-col items-center gap-2 p-4 rounded-full ${className}`}
+      className={`absolute flex flex-col items-center z-20 gap-2 p-4 rounded-full ${className}`}
     >
       <div
         className={`bg-${fg.hue}-${fg.value} w-12 h-16 rounded-full text-${bg.hue}-${bg.value} flex justify-center items-center cursor-pointer`}
-        onClick={() => {
-          setLValue(Lvalue + 0.01);
-          setRValue(Rvalue + 0.01);
-        }}
+        onClick={() =>
+          window.dispatchEvent(new KeyboardEvent("keydown", { key: "w" }))
+        }
       >
         w
       </div>
       <div className="flex gap-60 items-center">
         <div
           className={`bg-${fg.hue}-${fg.value} w-12 h-16 rounded-full text-${bg.hue}-${bg.value} flex justify-center items-center cursor-pointer`}
-          onClick={() => setLValue(Lvalue + 0.01)}
+          onClick={() =>
+            window.dispatchEvent(new KeyboardEvent("keydown", { key: "a" }))
+          }
         >
           a
         </div>
         <div
           className={`bg-${fg.hue}-${fg.value} w-12 h-12 rounded-full text-${bg.hue}-${bg.value} flex justify-center items-center cursor-pointer`}
-          onClick={() => {
-            setLValue(0);
-            setRValue(0);
-          }}
+          onClick={() =>
+            window.dispatchEvent(new KeyboardEvent("keydown", { key: "s" }))
+          }
         >
           s
         </div>
         <div
           className={`bg-${fg.hue}-${fg.value} w-12 h-16 rounded-full text-${bg.hue}-${bg.value} flex justify-center items-center cursor-pointer`}
-          onClick={() => setRValue(Rvalue + 0.01)}
+          onClick={() =>
+            window.dispatchEvent(new KeyboardEvent("keydown", { key: "d" }))
+          }
         >
           d
         </div>
       </div>
       <div
         className={`bg-${fg.hue}-${fg.value} w-12 h-16 rounded-full text-${bg.hue}-${bg.value} flex justify-center items-center cursor-pointer`}
-        onClick={() => {
-          setLValue(Lvalue - 0.01);
-          setRValue(Rvalue - 0.01);
-        }}
+        onClick={() =>
+          window.dispatchEvent(new KeyboardEvent("keydown", { key: "x" }))
+        }
       >
         x
       </div>
