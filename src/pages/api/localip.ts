a9a2ba59377cd7e1d2a7c974e2b932500d64c8c1
lucidmach <lucidmach@gmail.com>
2023-03-06 14:48:51 +0900

    ts supported lib for local ip

--- src/pages/api/localip.ts
+++ src/pages/api/localip.ts
@@ -1,15 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import localIpAddress from "local-ip-address";
+import { internalIpV4 } from "internal-ip";
 
 type Data = {
   ip: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const ip = localIpAddress();
-  res.status(200).json({ ip });
+  const ip = await internalIpV4();
+  if (ip) res.status(200).json({ ip });
+  return res
+    .status(200)
+    .json({ ip: "server error couldn't find local IP v4 address" });
 }
