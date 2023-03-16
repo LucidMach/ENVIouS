44ec64d3e8a6516488d9642c1eb7faf8fbab428e
lucidmach <lucidmach@gmail.com>
2023-03-16 15:22:29 +0900

    setup script

---
+++ scripts/ros-foxy.setup.sh
@@ -0,0 +1,33 @@
+wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros2_foxy.sh
+sudo chmod 755 ./install_ros2_foxy.sh
+bash ./install_ros2_foxy.sh
+
+sudo apt install ros-foxy-navigation2
+sudo apt install ros-foxy-nav2-bringup
+
+sudo apt install ros-foxy-dynamixel-sdk
+sudo apt install ros-foxy-turtlebot3-msgs
+sudo apt install ros-foxy-turtlebot3
+
+sudo apt install ros-foxy-rosbridge-server
+sudo apt-get install ros-foxy-v4l2-camera
+
+
+echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
+echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
+source /opt/ros/foxy/setup.bash
+source ~/.bashrc
+
+sudo dpkg --add-architecture armhf
+sudo apt update
+sudo apt install libc6:armhf
+
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=burger
+rm -rf ./opencr_update.tar.bz2
+
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+tar -xvf ./opencr_update.tar.bz2
+
+cd ~/opencr_update
+./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
